5e4867e0cc46b5aaa6d1636ef64487fbe3829ea1
eblondel <emmanuel.blondel1@gmail.com>
2020-02-25 08:00:59 +0100

    rename to load_dataset.R (in principle applicable to all datasets)

--- tunaatlas_actions/load_dataset.R
+++ tunaatlas_actions/load_dataset.R
@@ -1,5 +1,6 @@
-load_rfmo_datasets <- function(entity, config, options){
+load_dataset <- function(entity, config, options){
 
+	#control to check that everything is ok on mappings side, if not we stop the workflow until mappings are fixed/updated
 	if(dir.exists("errors_mappings")){
 		errMsg <- "Hum, It seems they are still missing codelist mappings! Cannot proceed with loading datasets. Aborting workflow..."
 		config$logger.error(errMsg)
