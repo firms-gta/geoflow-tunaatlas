9222042f8da3e5d3289a4e958768dfe960b1730e
Barde <juldebar@ordibibi3>
2020-03-25 12:58:54 +0400

    add actions to create materialized views

--- tunaatlas_actions/load_all_dataset.R
+++ tunaatlas_actions/load_all_dataset.R
@@ -3,8 +3,8 @@ load_all_dataset <- function(entity, config, options){
   source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/load_codelist.R")
   source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/load_dataset.R")
   source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/load_mapping.R")
-  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/create_codelist_materialized_view")
-  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/create_mappings_materialized_view")
+  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/create_codelist_materialized_view.R")
+  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/create_mappings_materialized_view.R")
   codelist_pid <- entity$identifiers[["id"]]
   
   if(!grepl(pattern = "mapping",x=codelist_pid) && !grepl(pattern = "level0", x=codelist_pid)){
@@ -16,5 +16,5 @@ load_all_dataset <- function(entity, config, options){
   } else if(grepl(pattern = "level0",x=codelist_pid)){
     load_dataset(entity, config, options)
   }
-	
+  
 }
