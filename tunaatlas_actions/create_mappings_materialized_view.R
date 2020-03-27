06fd9d5ad54ffe5204dad64846ad8c2b73849ee5
Barde <juldebar@ordibibi3>
2020-03-27 12:22:11 +0400

    add SQL comments and fix bug

--- tunaatlas_actions/create_mappings_materialized_view.R
+++ tunaatlas_actions/create_mappings_materialized_view.R
@@ -18,7 +18,8 @@ create_mappings_materialized_view <- function(entity, config, options){
       
       this_view <- paste0(views$table_schema[v],".",views$table_name[v])
       view_def <- dbGetQuery(CON,paste0("SELECT definition FROM (select pg_get_viewdef('",this_view,"', true) AS definition) AS view_def"))
-      view_comment <- dbGetQuery(CON,paste0("select c.relname table_name, pg_catalog.obj_description(c.oid) as comment from pg_catalog.pg_class c where c.relname = '",views$table_name[v],"';"))
+      view_comment <- dbGetQuery(CON,paste0("SELECT c.relname table_name, pg_catalog.obj_description(c.oid) as comment 
+                                            FROM pg_catalog.pg_class c where c.relname = '",views$table_name[v],"';"))
       sql_columns_comments <- paste0("SELECT a.attname As column_name,  d.description
    FROM pg_class As c
     INNER JOIN pg_attribute As a ON c.oid = a.attrelid
@@ -36,7 +37,9 @@ create_mappings_materialized_view <- function(entity, config, options){
       config$logger.info(sprintf("\n SQL mat view '%s' ",sql_mat_view))
       create_materialized_view <- dbGetQuery(CON,sql_mat_view) 
       
-      config$logger.info(sprintf("\n Add comments for view '%s'",views$table_name[v]))
+      config$logger.info(sprintf("\n Add comments for view '%s' and related columns ",views$table_name[v]))
+      sql_view_comment <- paste0("COMMENT ON MATERIALIZED VIEW ",this_view," IS '",view_comment$comment[1],"';")
+      dbGetQuery(CON,sql_view_comment)
       
       for(c in 1:nrow(view_columns_comments)){
         this_column <-paste0(this_view,".",view_columns_comments$column_name[c])
