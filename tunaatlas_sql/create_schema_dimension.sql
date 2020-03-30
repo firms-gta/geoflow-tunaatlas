916fd0c50e54a4817ff307d6d7ae63a45824dff5
Barde <juldebar@ordibibi3>
2020-03-30 18:13:28 +0400

    add comments on dimensions tables

--- tunaatlas_sql/create_schema_dimension.sql
+++ tunaatlas_sql/create_schema_dimension.sql
@@ -26,11 +26,11 @@ CREATE TABLE %dimension_name%.%dimension_name%
 ALTER TABLE %dimension_name%.%dimension_name%
   OWNER TO "%db_admin%";
 GRANT ALL ON TABLE %dimension_name%.%dimension_name% TO "%db_admin%";
-COMMENT ON TABLE  %dimension_name%.%dimension_name%  IS '"%dimension_name%.%dimension_name%" is a dimension of the data warehouse: a list of codes which gives the context of the values stored in the fact table ';
-COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."id_%dimension_name%" IS '"id_%dimension_name%"  column identifies (primary key) the %dimension_name% with a numeric code (integer)';
-COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."codesource_%dimension_name%" IS '"codesource_%dimension_name%" indicates what is the label in the dataset where this code comes from.';
-COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."tablesource_%dimension_name%" IS '"tablesource_%dimension_name%" gives the name of the dataset from which dataset this code comes ';
-COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."id_metadata" IS '"id_metadata" indicates from which dataset this code comes from by pointing the identifier (foreign key) of the related metadata in the metadata table';
+COMMENT ON TABLE  %dimension_name%.%dimension_name%  IS '"%dimension_name%.%dimension_name%" is a dimension of the data warehouse: a list of codes which gives the context of the values stored in the fact table. Paul: This table exists for each dimension (1 table by dimension available in the DW). It gathers all the primary keys (i.e. codes) of all the reference data of the dimension. It enables to make the link between the reference data and the fact tables that use these reference data (see DW documentation for more details).';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."id_%dimension_name%" IS '"id_%dimension_name%"  column identifies (primary key) the %dimension_name% with a numeric code (integer). Paul: Primary key. Technical numerical identifier for each code used in the reference table of the dimension.';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."codesource_%dimension_name%" IS '"codesource_%dimension_name%" indicates what is the label in the dataset where this code comes from. Paul: Conceptually references each reference table of the dimension (the reference is not technically implemented in the physical model). Code of the reference table.';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."tablesource_%dimension_name%" IS '"tablesource_%dimension_name%" gives the name of the dataset from which dataset this code comes. Paul: Identifier of the table that contains the code.';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%"."id_metadata" IS '"id_metadata" indicates from which dataset this code comes from by pointing the identifier (foreign key) of the related metadata in the metadata table. Paul: Foreign key - References the table metadata. Metadata of the reference table that contains the code.';
 
 
 
@@ -55,10 +55,11 @@ CREATE TABLE %dimension_name%.%dimension_name%_mapping
 ALTER TABLE %dimension_name%.%dimension_name%_mapping
   OWNER TO "%db_admin%";
 GRANT ALL ON TABLE "%dimension_name%"."%dimension_name%_mapping" TO "%db_admin%";
-COMMENT ON TABLE  "%dimension_name%"."%dimension_name%_mapping"  IS '"%dimension_name%"."%dimension_name%_mapping" enables to swicth from a codification system to another. For example, this table is required to generate global datasets from regional datasets which use specific code lists.';
-COMMENT ON COLUMN "%dimension_name%"."%dimension_name%_mapping"."%dimension_name%_mapping_id_from" IS '"%dimension_name%_mapping_id_from"  is the identifier (foreign key) of the code which is translated by this mapping relationship into another codification system';
-COMMENT ON COLUMN "%dimension_name%"."%dimension_name%_mapping"."%dimension_name%_mapping_id_to" IS '"%dimension_name%_mapping_id_to" gives the identifier (foreign key) of the code which results from the mapping';
-COMMENT ON COLUMN "%dimension_name%"."%dimension_name%_mapping"."%dimension_name%_mapping_relation_type" IS '"%dimension_name%_mapping_relation_type" specifies the kind / semantic of the relationship: same as, generalizes..';
+COMMENT ON TABLE  "%dimension_name%"."%dimension_name%_mapping"  IS '"%dimension_name%"."%dimension_name%_mapping" enables to swicth from a codification system to another. For example, this table is required to generate global datasets from regional datasets which use specific code lists. Paul: This table exists for each dimension (1 table by dimension available in the DW). It stores the eventual mappings (i.e. correspondences) between code lists belonging to the dimension.';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%_mapping"."%dimension_name%_mapping_id_from" IS '"%dimension_name%_mapping_id_from"  is the identifier (foreign key) of the code which is translated by this mapping relationship into another codification system. Paul: Foreign key - References the table <dimension>. id (from the table <dimension>) of the source dataset.';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%_mapping"."%dimension_name%_mapping_id_to" IS '"%dimension_name%_mapping_id_to" gives the identifier (foreign key) of the code which results from the mapping. Paul: Foreign key - References the table %dimension_name% id (from the table <dimension>) of the target dataset.';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%_mapping"."%dimension_name%_mapping_relation_type" IS '"%dimension_name%_mapping_relation_type" specifies the kind / semantic of the relationship: same as, generalizes.. Paul : Type of relation that exists between the source code and the target code (e.g. is strictly equivalent to, is part of, etc.).';
+COMMENT ON COLUMN "%dimension_name%"."%dimension_name%_mapping"."id_metadata" IS '"id_metadata"...Paul : Foreign key - References the table metadata. Metadata of the mapping.';
 
 
 INSERT INTO %dimension_name%.%dimension_name%(id_%dimension_name%,codesource_%dimension_name%) VALUES (0,'UNK');
@@ -79,6 +80,7 @@ COMMENT ON COLUMN %dimension_name%.%dimension_name%_labels.id_%dimension_name% I
 COMMENT ON COLUMN %dimension_name%.%dimension_name%_labels.codesource_%dimension_name% IS '"codesource_%dimension_name%" the label of the code in the dataset where this code comes from';
 COMMENT ON COLUMN %dimension_name%.%dimension_name%_labels.tablesource_%dimension_name% IS '"tablesource_%dimension_name%" the name of the dataset from which dataset this code comes';
 COMMENT ON COLUMN %dimension_name%.%dimension_name%_labels.source_label IS '"source_label" the label of the code in the dataset where this code comes from';
+COMMENT ON COLUMN %dimension_name%.%dimension_name%_labels.source_french_label IS '"source_french_label" the french label of the code in the dataset where this code comes from';
 COMMENT ON COLUMN %dimension_name%.%dimension_name%_labels.source_spanish_label IS '"source_spanish_label" the spanish label of the code in the dataset where this code comes from';
     
 
@@ -116,4 +118,4 @@ COMMENT ON COLUMN %dimension_name%.%dimension_name%_mapping_view.src_code IS '"s
 COMMENT ON COLUMN %dimension_name%.%dimension_name%_mapping_view.trg_code IS '"trg_code" the label of the code which results from the mapping';
 COMMENT ON COLUMN %dimension_name%.%dimension_name%_mapping_view.src_codingsystem IS '"src_codingsystem" the name of the dataset from which dataset this code comes';
 COMMENT ON COLUMN %dimension_name%.%dimension_name%_mapping_view.trg_codingsystem IS '"trg_codingsystem" the name of the standard codelist which is the ouput of the mapping';
-COMMENT ON COLUMN %dimension_name%.%dimension_name%_mapping_view.trg_codingsystem IS '"relation_type" the kind of relationship betweend these two codes: same as, generalizes..';
+COMMENT ON COLUMN %dimension_name%.%dimension_name%_mapping_view.relation_type IS '"relation_type" the kind of relationship between these two codes: same as, generalizes..';
