4c69adcaf220f2b890f6f61b5a1a9dce4e0195ad
eblondel <emmanuel.blondel1@gmail.com>
2021-12-06 00:22:10 +0100

    fix sql

--- tunaatlas_sql/create_view_area_labels_viewer.sql
+++ tunaatlas_sql/create_view_area_labels_viewer.sql
@@ -3,16 +3,16 @@
 -- To improve the dataset views, 1deg and 5deg grids are cut by continent.
 
 CREATE MATERIALIZED VIEW area.grid_area_labels AS
-SELECT * FROM area.area_labels WHERE tablesource_area = 'areas_tuna_rfmos_task2';
+SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_difference(ar.geom, cont.geometry) as geom FROM area.area_labels as ar, public.continent as cont WHERE tablesource_area = 'areas_tuna_rfmos_task2' AND st_intersects(ar.geom, cont.geometry);
 CREATE INDEX grid_area_labels_id_area_idx  ON area.grid_area_labels  (id_area);
 CREATE INDEX grid_area_labels_codesource_area_idx  ON area.grid_area_labels (codesource_area);
 
 CREATE MATERIALIZED VIEW area.grid_5deg_area_labels as 
-SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_difference(ar.geom, cont.geometry) as geom FROM area.grid_area_labels as ar, public.continent as cont WHERE ar.codesource_area like '6%' and st_intersects(ar.geom, cont.geometry);
-CREATE INDEX grid_area_5deg_labels_id_area_idx  ON area.grid_5deg_area_labels  (id_area);
+SELECT * FROM area.grid_area_labels WHERE codesource_area like '6%';
+CREATE INDEX grid_area_5deg_labels_id_area_idx  ON area.grid_5deg_area_labels (id_area);
 CREATE INDEX grid_area_5deg_labels_codesource_area_idx  ON area.grid_5deg_area_labels (codesource_area);
 
 CREATE MATERIALIZED VIEW area.grid_1deg_area_labels as
-SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_difference(ar.geom, cont.geometry) as geom FROM area.grid_area_labels as ar, public.continent as cont WHERE ar.codesource_area like '5%' and st_intersects(ar.geom, cont.geometry);
-CREATE INDEX grid_area_1deg_labels_id_area_idx  ON area.grid_1deg_area_labels  (id_area);
+SELECT * FROM area.grid_area_labels WHERE codesource_area like '5%';
+CREATE INDEX grid_area_1deg_labels_id_area_idx  ON area.grid_1deg_area_labels (id_area);
 CREATE INDEX grid_area_1deg_labels_codesource_area_idx  ON area.grid_1deg_area_labels (codesource_area);
