3814b8903f438114346d6249488417f0cd54ee5c
eblondel <emmanuel.blondel1@gmail.com>
2021-12-05 22:00:46 +0100

    create_view_area_labels_viewer.sql and create_grid_views.R ton run on 'onend_r' in dbmodel+codelists (only for viewer)

--- tunaatlas_sql/create_view_area_labels_viewer.sql
+++ tunaatlas_sql/create_view_area_labels_viewer.sql
@@ -1,20 +1,18 @@
 -- Grid area labels for dataset views in Tuna atlas map viewer
 -- These materialized views should be managed once the database area labels are loaded
--- To improve the dataset views, grids are cut by continent.
+-- To improve the dataset views, 1deg and 5deg grids are cut by continent.
 
-DROP VIEW IF EXISTS area.grid_area_labels;
 CREATE MATERIALIZED VIEW area.grid_area_labels AS
 SELECT * FROM area.area_labels WHERE tablesource_area = 'areas_tuna_rfmos_task2';
 CREATE INDEX grid_area_labels_id_area_idx  ON area.grid_area_labels  (id_area);
 CREATE INDEX grid_area_labels_codesource_area_idx  ON area.grid_area_labels (codesource_area);
 
-DROP VIEW IF EXISTS area.grid_5deg_area_labels;
 CREATE MATERIALIZED VIEW area.grid_5deg_area_labels as 
-SELECT * FROM area.grid_area_labels where codesource_area like '6%';
+SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_intersects(ar.geom, cont.geometry) as geom FROM area.grid_area_labels as ar, public.continent as cont WHERE ar.codesource_area like '6%' and st_intersects(ar.geom, cont.geometry);
 CREATE INDEX grid_area_5deg_labels_id_area_idx  ON area.grid_5deg_area_labels  (id_area);
 CREATE INDEX grid_area_5deg_labels_codesource_area_idx  ON area.grid_5deg_area_labels (codesource_area);
 
-DROP VIEW IF EXISTS area.grid_1deg_area_labels;
-CREATE MATERIALIZED VIEW area.grid_1deg_area_labels as SELECT * FROM area.grid_area_labels where codesource_area like '5%';
+CREATE MATERIALIZED VIEW area.grid_1deg_area_labels as
+SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_intersects(ar.geom, cont.geometry) as geom FROM area.grid_area_labels as ar, public.continent as cont WHERE ar.codesource_area like '5%' and st_intersects(ar.geom, cont.geometry);
 CREATE INDEX grid_area_1deg_labels_id_area_idx  ON area.grid_1deg_area_labels  (id_area);
 CREATE INDEX grid_area_1deg_labels_codesource_area_idx  ON area.grid_1deg_area_labels (codesource_area);
