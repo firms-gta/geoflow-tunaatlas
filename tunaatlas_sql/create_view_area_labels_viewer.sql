7798366118505b0f7beda1e4bf9fc50278c751ed
eblondel <emmanuel.blondel1@gmail.com>
2021-12-21 09:10:58 +0100

    add task1 areas view erased by continent (for viewer)

--- tunaatlas_sql/create_view_area_labels_viewer.sql
+++ tunaatlas_sql/create_view_area_labels_viewer.sql
@@ -1,6 +1,11 @@
--- Grid area labels for dataset views in Tuna atlas map viewer
+-- Area labels for dataset views in Tuna atlas map viewer
 -- These materialized views should be managed once the database area labels are loaded
 -- To improve the dataset views, 1deg and 5deg grids are cut by continent.
+
+CREATE MATERIALIZED VIEW area.erased_area_labels AS
+SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_difference(ar.geom, cont.geometry) as geom FROM area.area_labels as ar, public.continent as cont WHERE tablesource_area = 'areas_tuna_rfmos_task1' AND st_intersects(ar.geom, cont.geometry);
+CREATE INDEX erased_area_labels_id_area_idx ON area.erased_area_labels (id_area);
+CREATE INDEX erased_area_labels_codesource_area_idx  ON area.erased_area_labels (codesource_area);
 
 CREATE MATERIALIZED VIEW area.grid_area_labels AS
 SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_difference(ar.geom, cont.geometry) as geom FROM area.area_labels as ar, public.continent as cont WHERE tablesource_area = 'areas_tuna_rfmos_task2' AND st_intersects(ar.geom, cont.geometry);
