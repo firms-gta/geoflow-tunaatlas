3a3741765f49c50c580e08471f66571206fea34d
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-09-29 11:20:39 +0200

    changing the source extension to rename as it is no longer a .mdb

--- tunaatlas_scripts/pre-harmonization/atlantic_ocean_catch_tunaatlasiccat_level0__noschool.R
+++ tunaatlas_scripts/pre-harmonization/atlantic_ocean_catch_tunaatlasiccat_level0__noschool.R
@@ -156,9 +156,9 @@ dataset_temporal_extent <- paste(
 entity$setTemporalExtent(dataset_temporal_extent)
 
 #@geoflow -> export as csv
-output_name_dataset <- gsub(filename1, paste0(unlist(strsplit(filename1,".mdb"))[1], "_harmonized.csv"), path_to_raw_dataset)
+output_name_dataset <- gsub(filename1, paste0(unlist(strsplit(filename1,".csv"))[1], "_harmonized.csv"), path_to_raw_dataset)
 write.csv(catches, output_name_dataset, row.names = FALSE)
-output_name_codelists <- gsub(filename1, paste0(unlist(strsplit(filename1,".mdb"))[1], "_codelists.csv"), path_to_raw_dataset)
+output_name_codelists <- gsub(filename1, paste0(unlist(strsplit(filename1,".csv"))[1], "_codelists.csv"), path_to_raw_dataset)
 file.rename(from = entity$getJobDataResource(config, filename2), to = output_name_codelists)
 #----------------------------------------------------------------------------------------------------------------------------
 entity$addResource("source", path_to_raw_dataset)
