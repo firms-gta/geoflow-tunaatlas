92458213cbffeccd18cea8284da255e5e1488771
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-10-26 11:41:24 +0200

    adding rtunaatlaspackage function to new repo

---
+++ sardara_functions/extract_dataset.R
@@ -0,0 +1,15 @@
+extract_dataset = function (con, metadata_dataset, labels = FALSE) 
+{
+  source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/getSQLSardaraQueries.R")
+  if (nrow(metadata_dataset) == 0) {
+    stop("There is no dataset that corresponds to your query")
+  }
+  if (labels == FALSE) {
+    query <- getSQLSardaraQueries(con, metadata_dataset)$query_CSV
+  }
+  else {
+    query <- getSQLSardaraQueries(con, metadata_dataset)$query_CSV_with_labels
+  }
+  df <- dbGetQuery(con, query)
+  return(df)
+}
