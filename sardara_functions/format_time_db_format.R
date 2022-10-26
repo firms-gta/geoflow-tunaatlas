5a074aea26f5aea3026a224d420d9773f3d78d06
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-10-26 14:19:56 +0200

    laoding lubridate

--- sardara_functions/format_time_db_format.R
+++ sardara_functions/format_time_db_format.R
@@ -1,5 +1,7 @@
 format_time_db_format = function (df_input) 
 {
+  if(!require(lubridate)){install.packages("lubridate")
+    require(lubridate)}
   df_input$time_start <- as.Date(paste(df_input[, "Year"], 
                                        "-", df_input[, "MonthStart"], "-01", sep = ""))
   df_input$time_end <- df_input$time_start + months(df_input[, 
