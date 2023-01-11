a77cf460a23b017de07f067c38f3d7f1f4b68b71
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-01-11 17:01:29 +0100

    - adding a recap of mapping as it was not working - changing new source of extract_dataset - fixing lines creating bugs

--- tunaatlas_scripts/generation/map_codelists.R
+++ tunaatlas_scripts/generation/map_codelists.R
@@ -1,15 +1,18 @@
 #re-written from https://raw.githubusercontent.com/ptaconet/rtunaatlas_scripts/master/tunaatlas_world/create_own_tuna_atlas/sourced_scripts/map_code_lists.R
-map_codelists <- function(con, fact, mapping_dataset,dataset_to_map, mapping_keep_src_code = FALSE, summary_mapping = FALSE){
+map_codelists<-function(con, fact, mapping_dataset,dataset_to_map, mapping_keep_src_code = FALSE, summary_mapping = FALSE){
   # Get the dimensions to map from the mapping_dataset
   source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/map_codelist.R")
-  source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/extract_dataset.R")
+  source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/extract_dataset.R")
   source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/list_metadata_datasets.R")
   
   if (fact=="catch"){
     dimension_to_map<-c("gear","species","fishingfleet","schooltype","catchtype")
+    dimension_to_map <- dimension_to_map[dimension_to_map%in%colnames(dataset_to_map)]
   } else if (fact=="effort"){
     dimension_to_map<-c("gear","fishingfleet","schooltype","unit")
+    dimension_to_map <- dimension_to_map[dimension_to_map%in%colnames(dataset_to_map)]
   }
+  recap_mapping <- NULL
   # One by one, map the dimensions
   for (i in 1:length(dimension_to_map)){ # i takes the values of the dimensions to map
     dimension <- dimension_to_map[i]
@@ -20,12 +23,13 @@ map_codelists <- function(con, fact, mapping_dataset,dataset_to_map, mapping_kee
         df_mapping<-extract_dataset(con,list_metadata_datasets(con,identifier=mapping_dataset_this_dimension$db_mapping_dataset_name[j]))  # Extract the code list mapping dataset from the DB
         df_mapping$source_authority<-mapping_dataset_this_dimension$source_authority[j]  # Add the dimension "source_authority" to the mapping dataset. That dimension is not included in the code list mapping datasets. However, it is necessary to map the code list.
         df_mapping_final_this_dimension<-rbind(df_mapping_final_this_dimension,df_mapping)
-        df_mapping <- rbind(df_mapping_final_this_dimension, df_mapping)
       }
+      recap_mapping <- rbind(df_mapping_final_this_dimension, recap_mapping)
+      
       dataset_to_map <- map_codelist(dataset_to_map,df_mapping_final_this_dimension,dimension,mapping_keep_src_code)$df  # Codes are mapped by tRFMOs (source_authority)
     }
   }
-  if(summary_mapping){dataset_mapped <- list(dataset_mapped = dataset_to_map, summary_mapping =df_mapping)
+  if(summary_mapping){dataset_mapped <- list(dataset_mapped = dataset_to_map, summary_mapping =recap_mapping)
   }else {
     dataset_mapped <- dataset_to_map
   }
