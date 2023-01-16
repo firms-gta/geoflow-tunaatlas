81f43887595c7e501086b100692253f35e9b7e4d
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-01-16 17:10:23 +0100

    fixing bug and fastering it

--- tunaatlas_scripts/generation/function_overlapped.R
+++ tunaatlas_scripts/generation/function_overlapped.R
@@ -9,7 +9,8 @@ function_overlapped =function(dataset, con, rfmo_to_keep, rfmo_not_to_keep,
     columns_to_keep<-c("source_authority","gear","fishingfleet","schooltype","time_start","time_end","geographic_identifier","unit","value")
   }
   if("year"%in%strata){
-    dataset <- dataset %>% mutate(year = lubridate::year(time_start))
+    dataset <- dataset %>% mutate(year = as.character(lubridate::year(time_start)))
+    columns_to_keep <- append(setdiff(columns_to_keep, c("time_start", "time_end")), "year")
   }
   strata <- intersect(strata, columns_to_keep)
   rfmo_to_keep_DT <- dataset %>% filter(source_authority == rfmo_to_keep)
@@ -22,12 +23,12 @@ function_overlapped =function(dataset, con, rfmo_to_keep, rfmo_not_to_keep,
   overlapping_kept <- rbind(rfmo_not_to_keep_without_equivalent, rfmo_to_keep_DT)
   
   if( removing_unk){# if we keep data from both rfmos we can remove data labelled as "UNK" as they are not precise enough and they can be duplicated
-    overlapping_kept_unk_removed <- overlapping_kept %>% dplyr::group_by(strata) %>%
-      mutate(overlap = n(source_authority))
+    overlapping_kept_unk_removed <- overlapping_kept %>% 
+      dplyr::group_by(across(append(strata, "source_authority"))) %>% dplyr::rowwise()%>% mutate(overlap = n_distinct(source_authority))
     
     overlapping_kept <- overlapping_kept_unk_removed %>% 
       rowwise() %>% 
-      filter(any(c_across(everything(.)) == "UNK") & overlap == 2)
+      filter(!(overlap == 2 && any(c_across(strata) == "UNK")))
   }
   georef_dataset <- rbind(rfmo_restant, overlapping_kept)
   rm(rfmo_to_keep_DT, rfmo_not_to_keep_DT, rfmo_restant, rfmo_not_to_keep_without_equivalent)
