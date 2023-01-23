38a060cea976b8d576e1504d7b3a479599569950
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-01-23 12:15:31 +0100

    - anti join added to keep all the data as some was removed in previous - case where potential errors in cwp grid handled

--- tunaatlas_scripts/generation/spatial_curation_upgrade_resolution.R
+++ tunaatlas_scripts/generation/spatial_curation_upgrade_resolution.R
@@ -12,7 +12,11 @@ spatial_curation_upgrade_resolution =function (con, df_input, resolution)
     df_input_to_leave_as_so <- inner_join(df_input, cwp_grid_data_with_resolution_to_upgrade, 
                                           by = "geographic_identifier")
     df_input_to_leave_as_so$geographic_identifier <- df_input_to_leave_as_so$code
-    df_input_to_leave_as_so$code <- NULL
+    df_input_to_leave_as_so$code <- NULL   
+    df_input_not_to_leave_as_so <- anti_join(df_input, cwp_grid_data_with_resolution_to_upgrade,
+                                             by = "geographic_identifier")
+    # df_input_not_to_leave_as_so$geographic_identifier <- df_input_not_to_leave_as_so$code
+    # df_input_not_to_leave_as_so$code <- NULL
   }
   else {
     df_input_to_leave_as_so <- NULL
@@ -21,25 +25,10 @@ spatial_curation_upgrade_resolution =function (con, df_input, resolution)
                                    cwp_grid_data_with_resolution_to_upgrade$geographic_identifier)
   area_changeresolution <- paste(unique(area_changeresolution), 
                                  collapse = "','")
-  areas_to_project_data_to_aggregate <- dbGetQuery(con, paste0("SELECT\n      left(a2.code,7) as input_geographic_identifier,\n      left(a1.code,7) as geographic_identifier_project\n      from\n      area.cwp_grid a1,\n      area.cwp_grid a2\n      where\n      a2.code IN ('", 
-                                                               area_changeresolution, "') and\n      a1.size_grid = '6' and a2.size_grid = '5' and \n      ST_Within(a2.geom, a1.geom)\n      UNION\n      SELECT\n      a2.code as input_geographic_identifier,\n      left(a1.code,7) as geographic_identifier_project\n      from\n      area.cwp_grid a1,\n      area.irregular_areas_task2_iotc a2\n      where\n      a2.code IN ('", 
-                                                               area_changeresolution, "') and\n      a1.size_grid='6' and \n      ST_Within(a2.geom, a1.geom)"))
-  if (nrow(areas_to_project_data_to_aggregate) > 0) {
-    df_input_to_aggregate <- inner_join(df_input, areas_to_project_data_to_aggregate, 
-                                        by = c(geographic_identifier = "input_geographic_identifier"))
-    df_input_to_aggregate <- df_input_to_aggregate %>% group_by_(.dots = setdiff(c(columns_df_input, 
-                                                                                   "geographic_identifier_project"), c("geographic_identifier", 
-                                                                                                                       "value"))) %>% summarise(value = sum(value))
-    df_input_to_aggregate$geographic_identifier <- df_input_to_aggregate$geographic_identifier_project
-    df_input_to_aggregate$geographic_identifier_project <- NULL
-  }
-  else {
-    df_input_to_aggregate = NULL
-  }
   areas_to_project_data_to_disaggregate <- dbGetQuery(con, 
                                                       paste0("SELECT\n    left(a2.code,7) as input_geographic_identifier,\n    left(a1.code,7) as geographic_identifier_project\n    from\n    area.cwp_grid a1,\n    area.cwp_grid a2\n    where\n    a2.code IN ( '", 
                                                              area_changeresolution, "') and\n    a1.size_grid='6' and a2.size_grid IN ('1','2','7','8','9') and \n    ST_Within(a1.geom, a2.geom)\n    UNION\n    SELECT\n    a2.code as input_geographic_identifier,\n    left(a1.code,7) as geographic_identifier_project\n    from\n    area.cwp_grid a1,\n    area.irregular_areas_task2_iotc a2\n    where\n    a2.code IN ( '", 
-                                                             area_changeresolution, "') and\n    a1.size_grid='6' and \n    ST_Within(a1.geom, a2.geom)"))
+                                                             area_changeresolution, "') and\n    a1.size_grid='6' and \n    ST_Within(a1.geom, a2.geom)")) %>% distinct()
   if (nrow(areas_to_project_data_to_disaggregate) > 0) {
     areas_sup_to_resolution_to_aggregate <- unique(areas_to_project_data_to_disaggregate$input_geographic_identifier)
     df_input_areas_sup_to_resolution_to_aggregate <- df_input %>% 
@@ -48,6 +37,31 @@ spatial_curation_upgrade_resolution =function (con, df_input, resolution)
   else {
     df_input_areas_sup_to_resolution_to_aggregate = NULL
   }
+  # con %>% tbl(in_schema("area", "cwp_grid"))%>% filter(code == "6130045") %>% collect()
+  # con %>% tbl(in_schema("area", "irregular_areas_task2_iotc"))%>%  collect()
+  # 
+  areas_to_project_data_to_aggregate <- dbGetQuery(con, paste0("SELECT\n      left(a2.code,7) as input_geographic_identifier,\n      left(a1.code,7) as geographic_identifier_project\n      from\n      area.cwp_grid a1,\n      area.cwp_grid a2\n      where\n      a2.code IN ('", 
+                                                               area_changeresolution, "') and\n      a1.size_grid = '6' and a2.size_grid = '5' and \n      ST_Within(a2.geom, a1.geom)\n      UNION\n      SELECT\n      a2.code as input_geographic_identifier,\n      left(a1.code,7) as geographic_identifier_project\n      from\n      area.cwp_grid a1,\n      area.irregular_areas_task2_iotc a2\n      where\n      a2.code IN ('", 
+                                                               area_changeresolution, "') and\n      a1.size_grid='6' and \n      ST_Within(a2.geom, a1.geom)"))
+  areas_to_project_data_to_aggregate_without_duplicate_from_disaggregate <- anti_join(areas_to_project_data_to_aggregate,areas_to_project_data_to_disaggregate)
+  
+  if (nrow(areas_to_project_data_to_aggregate) > 0) {
+    
+    df_input_to_aggregate <- inner_join(df_input_not_to_leave_as_so, areas_to_project_data_to_aggregate_without_duplicate_from_disaggregate, 
+                                        by = c(geographic_identifier = "input_geographic_identifier")) # %>%  #left join beacause there is an error for the 6130045 cwp grid "6130045\n"
+    # mutate(geographic_identifier_project = ifelse(!is.na(geographic_identifier_project), geographic_identifier_project,geographic_identifier))
+    
+    df_input_to_aggregate <- df_input_to_aggregate %>% group_by_(.dots = setdiff(c(columns_df_input, 
+                                                                                   "geographic_identifier_project"), c("geographic_identifier", 
+                                                                                                                       "value"))) %>% summarise(value = sum(value))
+    df_input_to_aggregate$geographic_identifier <- df_input_to_aggregate$geographic_identifier_project
+    df_input_to_aggregate$geographic_identifier_project <- NULL
+    
+  }
+  else {
+    df_input_to_aggregate = NULL
+  }
+  
   df_input_final_aggregated_on_resolution_to_aggregate <- rbind(data.frame(df_input_to_leave_as_so), 
                                                                 data.frame(df_input_to_aggregate), data.frame(df_input_areas_sup_to_resolution_to_aggregate))
   if (!is.null(df_input_to_aggregate)) {
