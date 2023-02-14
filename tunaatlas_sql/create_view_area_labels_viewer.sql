4813e4bd5e3871fc3da9bda53489cc44a09d7883
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-02-14 14:27:53 +0100

    indexing area_labels to make long queries faster

--- tunaatlas_sql/create_view_area_labels_viewer.sql
+++ tunaatlas_sql/create_view_area_labels_viewer.sql
@@ -3,6 +3,7 @@
 -- To improve the dataset views, 1deg and 5deg grids are cut by continent.
 
 CREATE INDEX continent_geom_idx ON public.continent USING GIST (the_geom);
+CREATE INDEX area_area_labels_geom_idx ON area.area_labels USING GIST (geom);
 
 CREATE MATERIALIZED VIEW area.erased_area_labels AS
 SELECT ar.id_area, ar.codesource_area, ar.tablesource_area, ar.source_label, st_difference(ar.geom, cont.the_geom) as geom FROM area.area_labels as ar, public.continent as cont WHERE tablesource_area = 'areas_tuna_rfmos_task1' ;
