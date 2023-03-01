f3f9a7d2da7d8265189c49c75411bf00e2a9e3ac
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-03-01 15:57:38 +0100

    adding reshape2 calling

--- map_codelist.R
+++ map_codelist.R
@@ -24,7 +24,7 @@ map_codelist = function (df_input, df_mapping, dimension_to_map, keep_src_code =
                                                                                            dimension_to_map]), "sum_value_not_mapped", "sum_value_mapped")) %>% 
     group_by(sum_mapped_unmapped, unit) %>% summarise(sum_value_by_dimension = sum(value))
   df_input[, dimension_to_map][which(is.na(df_input[, dimension_to_map]))] = "UNK"
-  stats_data_not_mapped <- dcast(setDT(stats_data_not_mapped), 
+  stats_data_not_mapped <- reshape2::dcast(setDT(stats_data_not_mapped), 
                                  unit ~ sum_mapped_unmapped, sum)
   if (!("sum_value_not_mapped" %in% colnames(stats_data_not_mapped))) {
     stats_data_not_mapped$sum_value_not_mapped = 0
