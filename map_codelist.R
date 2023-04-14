e70e9a171e52eb502d42b81ca462ea1a10f8d795
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-04-14 15:38:40 +0400

    minor changes

--- map_codelist.R
+++ map_codelist.R
@@ -10,8 +10,8 @@ map_codelist = function (df_input, df_mapping, dimension_to_map, keep_src_code =
   df_input <- left_join(df_input, df_mapping)
   
   not_mapped <- unique((df_input %>% filter(is.na(trg_code))) %>% select({{dimension_to_map}})) %>% 
-    rename("Value" :={{dimension_to_map}} ) %>% 
-    mutate("Dimension" = dimension_to_map)
+    dplyr::rename("Value" :={{dimension_to_map}} ) %>% 
+    dplyr::mutate("Dimension" = dimension_to_map)
   if (keep_src_code == FALSE) {
     df_input[, dimension_to_map] <- df_input$trg_code
     df_input <- df_input[column_names_df_input]
@@ -31,7 +31,9 @@ map_codelist = function (df_input, df_mapping, dimension_to_map, keep_src_code =
   stats_data_not_mapped <- df_input %>% dplyr::mutate(sum_mapped_unmapped = ifelse(is.na(df_input[, 
                                                                                                   dimension_to_map]), "sum_value_not_mapped", "sum_value_mapped")) %>% 
     group_by(sum_mapped_unmapped, unit) %>% summarise(sum_value_by_dimension = sum(value))
+  
   df_input[, dimension_to_map][which(is.na(df_input[, dimension_to_map]))] = "UNK"
+  
   stats_data_not_mapped <- reshape2::dcast(setDT(stats_data_not_mapped), 
                                            unit ~ sum_mapped_unmapped, sum)
   if (!("sum_value_not_mapped" %in% colnames(stats_data_not_mapped))) {
@@ -41,6 +43,6 @@ map_codelist = function (df_input, df_mapping, dimension_to_map, keep_src_code =
   stats_data_not_mapped$percentage_not_mapped <- stats_data_not_mapped$sum_value_not_mapped/stats_data_not_mapped$sum_value_mapped * 
     100
   stats_data_not_mapped <- stats_data_not_mapped  %>% 
-    mutate("Dimension" = dimension_to_map)
+    dplyr::mutate("Dimension" = dimension_to_map)
   return(list(df = df_input, stats = stats_data_not_mapped, not_mapped = not_mapped))
 }
