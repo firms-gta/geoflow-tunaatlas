37d690055e8c13ca7f61db1cec5dd40d43c66a0e
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-04-25 14:53:30 +0400

    minor changes

--- tunaatlas_scripts/generation/map_codelists.R
+++ tunaatlas_scripts/generation/map_codelists.R
@@ -46,9 +46,6 @@ map_codelists<-function(con, fact, mapping_dataset,dataset_to_map, mapping_keep_
   
   dataset_mapped <- rbind(dataset_to_map, data_not_to_map)
   
-  if(summary_mapping){dataset_mapped <- list(dataset_mapped = dataset_mapped, summary_mapping =recap_mapping, stats_total = stats_total, not_mapped_total = not_mapped_total)
-  }else {
-    dataset_mapped <- dataset_mapped
-  }
+  dataset_mapped <- list(dataset_mapped = dataset_mapped, recap_mapping =recap_mapping, stats_total = stats_total, not_mapped_total = not_mapped_total)
   return(dataset_mapped)
 }
