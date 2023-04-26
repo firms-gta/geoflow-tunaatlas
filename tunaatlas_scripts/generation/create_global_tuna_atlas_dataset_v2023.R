26a4f7fa6de2ec68958363511fa5b89641f3d9ac
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-04-26 17:51:34 +0400

    fixing bug due to missing catchtype column in new datasets 2023

--- tunaatlas_scripts/generation/create_global_tuna_atlas_dataset_v2023.R
+++ tunaatlas_scripts/generation/create_global_tuna_atlas_dataset_v2023.R
@@ -568,19 +568,34 @@ and groups of gears.", "map_codelists", list(options_mapping_map_code_lists))
   }
   
   if (!is.null(opts$curation_absurd_converted_data)){
+    
+    colnames_georef_dataset_groupping <- setdiff(colnames(georef_dataset), c("value", "unit"))
+
     max_conversion_factor <- read.csv("data/max_conversion_factor.csv")
     strata_nomt <- georef_dataset %>% filter(unit%in%c("NOMT"))
     strata_mtno <- georef_dataset %>% filter(unit%in%c("MTNO"))
     strata_converted_level0 <-  rbind(strata_nomt, strata_mtno) %>% ungroup() %>% dplyr::select(-c(value)) %>% dplyr::distinct()
-    conversion_factor_level0 <- rbind(dplyr::inner_join(strata_nomt , strata_mtno, by = setdiff(colnames(strata_mtno), c("value", "unit") )) %>% dplyr::rename(NO =value.x, MT = value.y) %>%
-                                        dplyr::group_by(gear, time_start, time_end, geographic_identifier, species, source_authority, schooltype, catchtype ,fishingfleet) %>%
-                                        dplyr::summarise(NO = sum(NO), MT = sum(MT)) %>% ungroup()#%>% dplyr::select(-c(unit.y, unit.x))
-                                      , dplyr::inner_join(strata_mtno , strata_nomt, by = setdiff(colnames(strata_mtno), c("value", "unit") )) %>% dplyr::rename(MT =value.x, NO = value.y)%>%
-                                        dplyr::group_by(gear, time_start, time_end, geographic_identifier, species, source_authority, schooltype, catchtype ,fishingfleet)%>%
-                                        dplyr::summarise(NO = sum(NO), MT = sum(MT)) %>%ungroup() #%>%  dplyr::select(-c(unit.y, unit.x))
-    )%>% dplyr::distinct()%>%
-      dplyr::group_by(gear, time_start, time_end, geographic_identifier, species, source_authority, schooltype, catchtype ,fishingfleet) %>%
-      dplyr::summarise(NO = sum(NO), MT = sum(MT)) %>% dplyr::mutate(conversion_factor = MT/NO) %>% dplyr::distinct()
+    conversion_factor_level0 <- rbind(
+      dplyr::inner_join(strata_nomt , strata_mtno, by = setdiff(colnames(strata_mtno), c("value", "unit") )) %>%
+        dplyr::rename(NO =value.x, MT = value.y) %>%
+        dplyr::group_by(across(colnames_georef_dataset_groupping)) %>%
+        dplyr::summarise(NO = sum(NO), MT = sum(MT)) %>% 
+        ungroup(),
+      dplyr::inner_join(strata_mtno , strata_nomt, by = setdiff(colnames(strata_mtno), c("value", "unit") )) %>%
+        dplyr::rename(MT =value.x, NO = value.y)%>%
+        dplyr::group_by(across(colnames_georef_dataset_groupping)) %>%
+        dplyr::summarise(NO = sum(NO), MT = sum(MT)) %>% 
+        ungroup()
+    ) %>% 
+      dplyr::distinct() %>%
+      dplyr::group_by(across(colnames_georef_dataset_groupping)) %>%
+      dplyr::summarise(NO = sum(NO), MT = sum(MT)) %>% 
+      dplyr::mutate(conversion_factor = MT/NO) %>% 
+      dplyr::distinct()
+    
+    
+    
+    
     conversion_factor_to_keep <- conversion_factor_level0 %>% left_join(max_conversion_factor, by = "species") %>% filter((conversion_factor < max_weight | conversion_factor > min_weight | is.na(max_weight) | is.na(min_weight)))
     # conversion_factor_not_to_keep <- conversion_factor_level0 %>% left_join(max_conversion_factor, by = "species") %>% filter(conversion_factor > max_weight | conversion_factor < min_weight | conversion_factor < 0.002 | conversion_factor > 0.5)
     georef_dataset_without_nomt <- georef_dataset %>% dplyr::filter(unit%in%c("MT", "NO","MTNO"))
