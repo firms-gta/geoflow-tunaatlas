9e5b5ecac6e2546790ca2304274fe52ce25b617c
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-05-15 13:22:52 +0400

    make it faster

--- sardara_functions/format_time_db_format.R
+++ sardara_functions/format_time_db_format.R
@@ -1,12 +1,16 @@
 format_time_db_format = function (df_input) 
 {
-  if(!require(lubridate)){install.packages("lubridate")
-    require(lubridate)}
-  if(!require(dplyr)){install.packages("dplyr")
-    require(dplyr)}
+  if(!require(lubridate)){
+    install.packages("lubridate")
+    require(lubridate)
+  }
+  if(!require(dplyr)){
+    install.packages("dplyr")
+    require(dplyr)
+  }
   
-  df_input <- df_input %>%rowwise() %>% dplyr::mutate(time_start = as.Date(paste(Year, "-", MonthStart, "-01", sep = "")))
-  df_input <- df_input %>%rowwise() %>% dplyr::mutate(time_end = as.Date(time_start + months(Period) - lubridate::days(1)))
+  df_input$time_start <- as.Date(paste(df_input$Year, "-", df_input$MonthStart, "-01", sep = ""))
+  df_input$time_end <- as.Date(df_input$time_start) + lubridate::month(df_input$Period) - lubridate::days(1)
   df_input$time_start <- as.character(df_input$time_start)
   df_input$time_end <- as.character(df_input$time_end)
   return(df_input)
