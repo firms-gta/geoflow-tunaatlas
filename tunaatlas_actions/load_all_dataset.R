2860f3ceb01cf5f9c67424a3230519bf78556907
eblondel <emmanuel.blondel1@gmail.com>
2023-06-14 11:53:01 +0200

    load_all_dataset.R rely on local scripts instead of loading github scripts

--- tunaatlas_actions/load_all_dataset.R
+++ tunaatlas_actions/load_all_dataset.R
@@ -1,10 +1,10 @@
 load_all_dataset <- function(action,entity, config){
   opts <- action$options
-  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/load_codelist.R")
-  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/load_dataset.R")
-  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/load_mapping.R")
-  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/create_codelist_materialized_view.R")
-  source("https://raw.githubusercontent.com/eblondel/geoflow-tunaatlas/master/tunaatlas_actions/create_mappings_materialized_view.R")
+  source(geoflow::get_config_resource_path(config, "./tunaatlas_actions/load_codelist.R"))
+  source(geoflow::get_config_resource_path(config, "./tunaatlas_actions/load_dataset.R"))
+  source(geoflow::get_config_resource_path(config, "./tunaatlas_actions/load_mapping.R"))
+  source(geoflow::get_config_resource_path(config, "./tunaatlas_actions/create_codelist_materialized_view.R"))
+  source(geoflow::get_config_resource_path(config, "./tunaatlas_actions/create_mappings_materialized_view.R"))
   codelist_pid <- entity$identifiers[["id"]]
   
   if(!grepl(pattern = "mapping",x=codelist_pid) && !grepl(pattern = "level0", x=codelist_pid)){
