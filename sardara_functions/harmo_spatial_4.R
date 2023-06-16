92e4871fdba00788bca590f99ede6d435f636f0d
eblondel <emmanuel.blondel1@gmail.com>
2023-06-16 12:04:14 +0200

    reformat harmo_spatial_4.R

--- sardara_functions/harmo_spatial_4.R
+++ sardara_functions/harmo_spatial_4.R
@@ -3,22 +3,10 @@ harmo_spatial_4 = function (df_input, colname_latitude, colname_longitude, colna
 {
   # function to create CWP grid from lat long provided by IATTC.
   source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/harmo_spatial_1.R")
-  indice.quad.4 <- which(df_input[, colname_longitude] >= 
-                           -180 & df_input[, colname_longitude] <= 0 & df_input[, 
-                                                                                colname_latitude] >= 0 & df_input[, colname_latitude] <= 
-                           90)
-  indice.quad.3 <- which(df_input[, colname_longitude] >= 
-                           -180 & df_input[, colname_longitude] <= 0 & df_input[, 
-                                                                                colname_latitude] >= -90 & df_input[, colname_latitude] <= 
-                           0)
-  indice.quad.2 <- which(df_input[, colname_longitude] >= 
-                           0 & df_input[, colname_longitude] <= 180 & df_input[, 
-                                                                               colname_latitude] >= -90 & df_input[, colname_latitude] <= 
-                           0)
-  indice.quad.1 <- which(df_input[, colname_longitude] >= 
-                           0 & df_input[, colname_longitude] <= 180 & df_input[, 
-                                                                               colname_latitude] >= 0 & df_input[, colname_latitude] <= 
-                           90)
+  indice.quad.4 <- which(df_input[, colname_longitude] > -180 & df_input[, colname_longitude] < 0 & df_input[, colname_latitude] > 0 & df_input[, colname_latitude] < 90)
+  indice.quad.3 <- which(df_input[, colname_longitude] > -180 & df_input[, colname_longitude] < 0 & df_input[, colname_latitude] > -90 & df_input[, colname_latitude] < 0)
+  indice.quad.2 <- which(df_input[, colname_longitude] > 0 & df_input[, colname_longitude] < 180 & df_input[, colname_latitude] > -90 & df_input[, colname_latitude] < 0)
+  indice.quad.1 <- which(df_input[, colname_longitude] > 0 & df_input[, colname_longitude] < 180 & df_input[, colname_latitude] > 0 & df_input[, colname_latitude] < 90)
   df_input$quadrant <- 9
   df_input[indice.quad.1, "quadrant"] <- 1
   df_input[indice.quad.2, "quadrant"] <- 2
@@ -26,34 +14,24 @@ harmo_spatial_4 = function (df_input, colname_latitude, colname_longitude, colna
   df_input[indice.quad.4, "quadrant"] <- 4
   
   # IATTC provide lat/lon of the centroid of the grid, we need to change them before creating CWP grid code corresponding
-  
   if (length(indice.quad.4)) {
-    df_input[indice.quad.4, colname_longitude] <- df_input[indice.quad.4, 
-                                                           colname_longitude] + df_input[indice.quad.4, colname_squaresize]/2
-    df_input[indice.quad.4, colname_latitude] <- df_input[indice.quad.4, 
-                                                          colname_latitude] - df_input[indice.quad.4, colname_squaresize]/2
+    df_input[indice.quad.4, colname_longitude] <- df_input[indice.quad.4, colname_longitude] + df_input[indice.quad.4, colname_squaresize]/2
+    df_input[indice.quad.4, colname_latitude] <- df_input[indice.quad.4, colname_latitude] - df_input[indice.quad.4, colname_squaresize]/2
   }
   if (length(indice.quad.2)) {
-    df_input[indice.quad.2, colname_longitude] <- df_input[indice.quad.2, 
-                                                           colname_longitude] - df_input[indice.quad.2, colname_squaresize]/2
-    df_input[indice.quad.2, colname_latitude] <- df_input[indice.quad.2, 
-                                                          colname_latitude] + df_input[indice.quad.2, colname_squaresize]/2
+    df_input[indice.quad.2, colname_longitude] <- df_input[indice.quad.2, colname_longitude] - df_input[indice.quad.2, colname_squaresize]/2
+    df_input[indice.quad.2, colname_latitude] <- df_input[indice.quad.2, colname_latitude] + df_input[indice.quad.2, colname_squaresize]/2
   }
   if (length(indice.quad.3)) {
-    df_input[indice.quad.3, colname_longitude] <- df_input[indice.quad.3, 
-                                                           colname_longitude] + df_input[indice.quad.3, colname_squaresize]/2
-    df_input[indice.quad.3, colname_latitude] <- df_input[indice.quad.3, 
-                                                          colname_latitude] + df_input[indice.quad.3, colname_squaresize]/2
+    df_input[indice.quad.3, colname_longitude] <- df_input[indice.quad.3, colname_longitude] + df_input[indice.quad.3, colname_squaresize]/2
+    df_input[indice.quad.3, colname_latitude] <- df_input[indice.quad.3, colname_latitude] + df_input[indice.quad.3, colname_squaresize]/2
   }
   if (length(indice.quad.1)) {
-    df_input[indice.quad.1, colname_longitude] <- df_input[indice.quad.1, 
-                                                           colname_longitude] - df_input[indice.quad.1, colname_squaresize]/2
-    df_input[indice.quad.1, colname_latitude] <- df_input[indice.quad.1, 
-                                                          colname_latitude] - df_input[indice.quad.1, colname_squaresize]/2
+    df_input[indice.quad.1, colname_longitude] <- df_input[indice.quad.1, colname_longitude] - df_input[indice.quad.1, colname_squaresize]/2
+    df_input[indice.quad.1, colname_latitude] <- df_input[indice.quad.1, colname_latitude] - df_input[indice.quad.1, colname_squaresize]/2
   }
   df_input[, colname_longitude] <- abs(df_input[, colname_longitude])
   df_input[, colname_latitude] <- abs(df_input[, colname_latitude])
-  df_input <- harmo_spatial_1(df_input, colname_longitude, 
-                              colname_latitude, "quadrant", ColCodeSquareSize)
+  df_input <- harmo_spatial_1(df_input, colname_longitude, colname_latitude, "quadrant", ColCodeSquareSize)
   return(df_input)
 }
