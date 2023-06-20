6ee2dca583ee3e4ec667f4bb45ae7f6608db2de9
eblondel <emmanuel.blondel1@gmail.com>
2023-06-21 01:00:59 +0200

    fix #33

--- sardara_functions/map_codelist.R
+++ sardara_functions/map_codelist.R
@@ -25,26 +25,26 @@ map_codelist = function (df_input, df_mapping, dimension_to_map, keep_src_code =
   }
   if (keep_src_code == FALSE) {
     df_input <- df_input %>% dplyr::group_by_(.dots = setdiff(column_names_df_input, 
-                                                              "value")) %>% dplyr::summarise(value = sum(value))
+                                                              "measurement_value")) %>% dplyr::summarise(measurement_value = sum(measurement_value))
   }
   df_input <- data.frame(df_input)
   stats_data_not_mapped <- df_input %>% dplyr::mutate(sum_mapped_unmapped = ifelse(is.na(df_input[, 
                                                                                                   dimension_to_map]), "sum_value_not_mapped", "sum_value_mapped")) %>% 
-    group_by(sum_mapped_unmapped, unit) %>% summarise(sum_value_by_dimension = sum(value))
+    group_by(sum_mapped_unmapped, measurement_unit) %>% summarise(sum_value_by_dimension = sum(measurement_value))
   
-  if(dimension_to_map=="fishingfleet"){
+  if(dimension_to_map=="fishing_fleet"){
     replace_unk <- "NEI"
   } else if(dimension_to_map=="species"){
     replace_unk <- "MZZ"
   }
-  if(dimension_to_map=="fishingfleet"){
+  if(dimension_to_map=="fishing_fleet"){
     replace_unk <- "99.9"
   } else {replace_unk <- "UNK"}
   
   df_input[, dimension_to_map][which(is.na(df_input[, dimension_to_map]))] = replace_unk
   
   stats_data_not_mapped <- reshape2::dcast(setDT(stats_data_not_mapped), 
-                                           unit ~ sum_mapped_unmapped, sum)
+                                           measurement_unit ~ sum_mapped_unmapped, sum)
   if (!("sum_value_not_mapped" %in% colnames(stats_data_not_mapped))) {
     stats_data_not_mapped$sum_value_not_mapped = 0
   }
--- tunaatlas_scripts/generation/map_codelists.R
+++ tunaatlas_scripts/generation/map_codelists.R
@@ -1,21 +1,20 @@
 #re-written from https://raw.githubusercontent.com/ptaconet/rtunaatlas_scripts/master/tunaatlas_world/create_own_tuna_atlas/sourced_scripts/map_code_lists.R
 map_codelists<-function(con, fact, mapping_dataset,dataset_to_map, mapping_keep_src_code = FALSE, summary_mapping = FALSE, source_authority_to_map = c("IATTC", "CCSBT", "WCPFC")){
   # Get the dimensions to map from the mapping_dataset
-  source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/map_codelist.R")
+  source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/saradara_functions/map_codelist.R")
   source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/extract_dataset.R")
   source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/list_metadata_datasets.R")
   
   if (fact=="catch"){
-    dimension_to_map<-c("gear","species","fishingfleet","schooltype","catchtype")
+    dimension_to_map<-c("gear_type","species","fishing_fleet","fishing_mode","measurement_type")
     dimension_to_map <- dimension_to_map[dimension_to_map%in%colnames(dataset_to_map)]
   } else if (fact=="effort"){
-    dimension_to_map<-c("gear","fishingfleet","schooltype","unit")
+    dimension_to_map<-c("gear_type","fishing_fleet","fishing_mode","measurement_unit")
     dimension_to_map <- dimension_to_map[dimension_to_map%in%colnames(dataset_to_map)]
   }
-  `%notin%` <- Negate(`%in%`)
-  data_not_to_map <- dataset_to_map %>% dplyr::filter(source_authority %notin% source_authority_to_map)
-  dataset_to_map <- dataset_to_map %>% dplyr::filter(source_authority %in% source_authority_to_map)
-  mapping_dataset <- mapping_dataset %>% dplyr::filter(source_authority %in% source_authority_to_map)
+  data_not_to_map <- dataset_to_map[!dataset_to_map$source_authority %in% source_authority_to_map,]
+  dataset_to_map <- dataset_to_map[dataset_to_map$source_authority %in% source_authority_to_map,]
+  mapping_dataset <- mapping_dataset[mapping_dataset$source_authority %in% source_authority_to_map,]
   
   recap_mapping <- NULL
   not_mapped_total <- NULL
@@ -28,19 +27,19 @@ map_codelists<-function(con, fact, mapping_dataset,dataset_to_map, mapping_keep_
       df_mapping_final_this_dimension<-NULL
       for (j in 1:nrow(mapping_dataset_this_dimension)){ # With this loop, we extract one by one, for 1 given dimension, the code list mapping datasets from the DB. The last line of the loop binds all the code list mappings datasets for this given dimension.
         df_mapping<-extract_dataset(con,list_metadata_datasets(con,identifier=mapping_dataset_this_dimension$db_mapping_dataset_name[j]))  # Extract the code list mapping dataset from the DB
-        df_mapping$source_authority<-mapping_dataset_this_dimension$source_authority[j]  # Add the dimension "source_authority" to the mapping dataset. That dimension is not included in the code list mapping datasets. However, it is necessary to map the code list.
+        if(!"source_authority" %in% colnames(df_mapping)) df_mapping$source_authority<-mapping_dataset_this_dimension$source_authority[j]  # Add the dimension "source_authority" to the mapping dataset. That dimension is not included in the code list mapping datasets. However, it is necessary to map the code list.
         df_mapping_final_this_dimension<-rbind(df_mapping_final_this_dimension,df_mapping)
       }
-      recap_mapping <- rbind(df_mapping_final_this_dimension, recap_mapping)
+      if(summary_mapping) recap_mapping <- rbind(df_mapping_final_this_dimension, recap_mapping)
       
       mapping <- map_codelist(dataset_to_map,df_mapping_final_this_dimension,dimension,mapping_keep_src_code)
       dataset_to_map <- mapping$df  # Codes are mapped by tRFMOs (source_authority)
-      stats <- mapping$stats
-      not_mapped <- mapping$not_mapped
-      
-      not_mapped_total <- rbind(not_mapped_total, not_mapped)
-      
-      stats_total <- rbind(stats_total, stats)
+      if(summary_mapping){
+		  stats <- mapping$stats
+		  not_mapped <- mapping$not_mapped
+		  not_mapped_total <- rbind(not_mapped_total, not_mapped)
+		  stats_total <- rbind(stats_total, stats)
+	  }
     }
   }
   
