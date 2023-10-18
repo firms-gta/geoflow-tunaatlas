e05a6cd5e08c451b4427fca8af678c0158120779
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-10-18 11:48:39 +0400

    Tidying data for comparison function

---
+++ Analysis_markdown/functions/tidying_GTA_data_for_comparison.R
@@ -0,0 +1,26 @@
+tidying_GTA_data_for_comparison = function(dataframe, shape = NULL, 
+species_group_dataframe = NULL, cl_cwp_gear_level2_dataframe = NULL ){
+  if(is_string(dataframe)){
+    dataframe <- readRDS(dataframe)
+  }
+  
+  if("geographic_identifier"%in%colnames(dataframe) & !is.null(shape)){
+    dataframe <- dataframe%>%  dplyr::left_join(shape, by = c("geographic_identifier"="CWP_CODE"))
+    print_map <- TRUE
+  } else {print_map <- FALSE}
+  
+  if("GRIDTYPE"%in%colnames(dataframe)){
+    dataframe <- dataframe%>%dplyr::mutate(GRIDTYPE = as.character(GRIDTYPE))
+  }
+  if(!is.null(species_group_dataframe)){
+    dataframe <- dataframe %>% dplyr::left_join(species_group_dataframe%>% dplyr::distinct(), by = c("species"))
+  }
+  if("gear_type" %in%colnames(dataframe) & !is.null(cl_cwp_gear_level2_dataframe) ){
+    dataframe <- dataframe %>% dplyr::left_join(cl_cwp_gear_level2_dataframe, by = c("gear_type" = "Code"))
+  }
+  
+  dataframe <- dataframe%>%dplyr::mutate(measurement_unit = dplyr::case_when(measurement_unit %in% c("MT","t","MTNO", "Tons")~ "Tons", 
+  measurement_unit %in% c("NO", "NOMT","no", "Number of fish")~"Number of fish", TRUE ~ measurement_unit)) 
+  
+  return(dataframe = dataframe)
+}
