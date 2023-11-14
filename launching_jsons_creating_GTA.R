3ee48feb4c24db0c46ff1e7a2d2040517e36d949
bastienird <bastien.grasset@ird.fr>
2023-11-14 15:00:48 +0000

    with summarising step

--- launching_jsons_creating_GTA.R
+++ launching_jsons_creating_GTA.R
@@ -123,6 +123,12 @@ executeWorkflow(here("tunaatlas_qa_datasets_iccat_effort.json")) # ok
 lapply(paste0("jobs/", list("tunaatlas_qa_global_datasets_catch", "tunaatlas_qa_global_datasets_effort")),
                                    dir.create)
 
+file.path <- executeWorkflow(here("tunaatlas_qa_global_datasets_catch.json"))
+source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/Developement/Analysis_markdown/Summarising_step.R")
+config <- initWorkflow(here("tunaatlas_qa_global_datasets_catch.json"))
+con <- config$software$output$dbi
+Summarising_step(main_dir = file.path, connectionDB = con, config  =config)
+
 
 executeWorkflow("tunaatlas_qa_global_datasets_catch.json")
 executeWorkflow(here("tunaatlas_qa_global_datasets_catch_new.json"))
