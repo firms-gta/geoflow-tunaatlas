2e17335fc92723b111f918bf0b085cb80ec7d896
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-11-29 11:36:49 +0400

    replacing rds with csv to make it faster

--- Analysis_markdown/functions/function_recap_each_step.R
+++ Analysis_markdown/functions/function_recap_each_step.R
@@ -10,7 +10,7 @@ function_recap_each_step=function(nom_dossier, nomrds, explication="",fonctions=
   somme_t <- sum(rds_t$measurement_value, na.rm = TRUE)
   somme_no <- sum(rds_no$measurement_value, na.rm = TRUE)
   lines <- nrow(nomrds)
-  write_csv(data.frame(somme_t, somme_no, lines), paste0(nom_dossier,"/sums.csv"))
+  fwrite(data.frame(somme_t, somme_no, lines), paste0(nom_dossier,"/sums.csv"))
   if (!is.null(option_list)){
     options_substi <- as.list(substitute(option_list))[-1]
     print(options_substi)
@@ -23,7 +23,7 @@ function_recap_each_step=function(nom_dossier, nomrds, explication="",fonctions=
   } else {options_written = "NONE"}
   options_written_total <- assign("options_written_total", paste0(options_written_total, options_written), envir = .GlobalEnv)
   explenation_total <- assign("explenation_total", paste0(explenation_total, explication), envir = .GlobalEnv)
-  saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
+  fwrite(nomrds,paste0(nom_dossier,"/rds.csv"))
   write(explication, paste0(nom_dossier,"/explication.txt")) 
   write(explenation_total, paste0(nom_dossier,"/explenation_total.txt")) 
   write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
