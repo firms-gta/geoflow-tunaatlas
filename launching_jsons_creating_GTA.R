9d5f94af9ff5dbfc49ea1c222d0ed2a116af3f65
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-11-29 11:41:48 +0400

    first draft documentation

--- launching_jsons_creating_GTA.R
+++ launching_jsons_creating_GTA.R
@@ -1,87 +1,52 @@
+# This script ensures that required R packages are installed and loaded.
 
-if(!require(renv)){
-    install.packages("renv")
-    require(renv)
+# Function to check, install, and load a package
+check_install_package <- function(package_name) {
+  if (!require(package_name, character.only = TRUE)) {
+    install.packages(package_name)
+    require(package_name, character.only = TRUE)
   }
-renv::restore()
-#
-if(!require(remotes)){
-  install.packages("remotes")
-  require(remotes)
 }
 
+# 'renv' for project-specific environments
+check_install_package("renv")
+renv::restore() # Restore the project library
 
+# General utility packages
+check_install_package("remotes")   # Package management
+check_install_package("tinytex")   # LaTeX support
+check_install_package("googledrive")  # Google Drive integration
+check_install_package("gsheet")    # Google Sheets integration
+check_install_package("readr")     # Data import
+check_install_package("plotrix")   # Plotting utilities
+check_install_package("janitor")   # Data cleaning
+check_install_package("dotenv")    # Environment variable management
+check_install_package("data.table") # Data manipulation (Note: Marked for removal)
+check_install_package("here") # Handling path
 
+# Database related packages
+check_install_package("RSQLite")    # SQLite interface
+check_install_package("RPostgreSQL") # PostgreSQL interface
+check_install_package("RPostgres")   # Alternative PostgreSQL interface
+check_install_package("DBI")        # Database interface
+check_install_package("rpostgis")   # PostGIS interface
 
-if(!require(tinytex)){
-  install.packages("tinytex")
-  require(tinytex)
-}
-if(!require(geoflow)){
+# Geospatial packages
+check_install_package("rgeos")     # Geospatial operations
+
+# Additional, project-specific packages
+# 'geoflow' package from GitHub for workflow management
+if (!require(geoflow)) {
   remotes::install_github("eblondel/geoflow")
-  require(geoflow)}
-
-if(!require(RSQLite)){
-  install.packages("RSQLite")
-  require(RSQLite)
-}
-if(!require(RPostgreSQL)){
-  install.packages("RPostgreSQL")
-  require(RPostgreSQL)
-}
-if(!require(RPostgres)){
-  install.packages("RPostgres")
-  require(RPostgres)
+  require(geoflow)
 }
 
-if(!require(googledrive)){
-  install.packages("googledrive")
-  require(googledrive)
-}
-if(!require(DBI)){
-  install.packages("DBI")
-  require(DBI)
-}
-if(!require(gsheet)){
-  install.packages("gsheet")
-  require(gsheet)
-}
-if(!require(readr)){
-  install.packages("readr")
-  require(readr)
-}
-if(!require(plotrix)){
-  install.packages("plotrix")
-  require(plotrix)
-}
-if(!require(rgeos)){
-  install.packages("rgeos")
-  require(rgeos)
-}
+# Note: This script assumes that the internet connection is available and
+# the CRAN/GitHub repositories are accessible for package installation.
 
+# Choose your .env in which you have stored you password for googledrive (if wanted) and database (mandatory)
 
-if(!require(rpostgis)){
-  install.packages("rpostgis")
-  require(rpostgis)
-}
-if(!require(janitor)){
-  install.packages("janitor")
-  require(janitor)
-}
-
-if(!require(dotenv)){
-  install.packages("dotenv")
-  require(dotenv)
-}
-
-if(!require(data.table)){
-  install.packages("data.table")
-  require(data.table)
-} #to be removed 
-
-require(here)
-
-default_file = ".env"
+default_file <- ".env"
 
 if(file.exists(here::here("geoserver_sdi_lab.env"))){
   default_file <- "geoserver_sdi_lab.env"
@@ -90,62 +55,49 @@ if(file.exists(here::here("geoserver_sdi_lab.env"))){
 if(file.exists(here("geoserver_cines.env"))){
   default_file <- here("geoserver_cines.env")
 } # as it is the one used on Blue Cloud project, for personal use replace .env with your personal one
-require(here)
 
 load_dot_env(file = here::here(default_file)) # to be replaced by the one used
 # load_dot_env(file = "~/Documents/Tunaatlas_level1/catch_local.env")# source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/cwp_grids.R")
 
-# Create usefull datasets: 
-# - Aggregation
-# - Disaggregation
-# - Continent shape
+# First step is creation of the database model and loading of the codelist (around 1/2 hour)
+db_model <- executeWorkflow(here("json/tunaatlas_qa_dbmodel+codelists.json")) 
 
-# As aggregation and disagreggation dataset are really long to create (around 1 to 2 days) they are stored in the googledrive and this is launched only if necessary, the code are here to keep track of the creation but these dataset should not change
+# Second step is the loading of the mappings (1h)
+mappings <- executeWorkflow(here("json/tunaatlas_qa_mappings.json"))
 
-executeWorkflow(here("tunaatlas_qa_dbmodel+codelists.json")) 
-executeWorkflow(here("tunaatlas_qa_mappings.json"))
+# Third step is pre-harmonizing the datasets provide by tRFMOs: This step is divided in 3 
+# substep depending on the type of the data:
 
+## Nominal data
+
+nominal_catch <- executeWorkflow(here("json/Nominal_catch.json"))
+
+## Georeferenced catch
+
+raw_data_georef <- executeWorkflow(here("json/All_raw_data_georef.json"))
+
+## Goereferenced effort
+
+raw_data_georef_effort <- executeWorkflow(here("json/All_raw_data_georef_effort.json"))
+
+
+## Summarising the invalid data for all the datasets pre-harmonized
 source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/Developement/Analysis_markdown/Checking_raw_files_markdown/Summarising_invalid_data.R")
+Summarising_invalid_data(raw_data_georef, connectionDB = con)
+Summarising_invalid_data(raw_data_georef_effort, connectionDB = con)
 
 
-# Pre-harmonizing 
+# Create 5 datasets catch and effort
 
-## nominal data
-
-file.path <- executeWorkflow(here("Nominal_catch.json"))
-config <- initWorkflow(here("Nominal_catch.json"))
-con <- config$software$output$dbi
-Summarising_invalid_data(file.path, connectionDB = con)
+tunaatlas_qa_global_datasets_catch_path <- executeWorkflow(here::here("json/tunaatlas_qa_global_datasets_catch.json"))
 
 
-
-## georef catch
-
-file.path <- executeWorkflow(here("All_raw_data_georef.json"))
-source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/Developement/Analysis_markdown/Checking_raw_files_markdown/Summarising_invalid_data.R")
-config <- initWorkflow(here("All_raw_data_georef.json"))
-con <- config$software$output$dbi
-Summarising_invalid_data(file.path, connectionDB = con)
-
-## georeferenced effort
-
-file.path <- executeWorkflow(here("All_raw_data_georef_effort.json"))
-source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/Developement/Analysis_markdown/Checking_raw_files_markdown/Summarising_invalid_data.R")
-config <- initWorkflow(here("All_raw_data_georef_effort.json"))
-con <- config$software$output$dbi
-Summarising_invalid_data(file.path, connectionDB = con)
-
-
-
-# Create 4 datasets catch and effort
-
-tunaatlas_qa_global_datasets_catch_path <- executeWorkflow(here::here("tunaatlas_qa_global_datasets_catch.json"))
+## Recapitulation of all the treatment done for each final dataset
 source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/Developement/Analysis_markdown/functions/Summarising_step.R")
-config <- initWorkflow(here::here("tunaatlas_qa_global_datasets_catch.json"))
-con <- config$software$output$dbi
+con <- initWorkflow(here("json/DB_connection.json"))$software$input$dbi
 Summarising_step(main_dir = tunaatlas_qa_global_datasets_catch_path, connectionDB = con, config  =config)
 
-#netcdf creation
+## Netcdf creation (24h for level 2)
 source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/Developement/tunaatlas_actions/convert_to_netcdf.R")
 entity_dirs <- list.dirs(file.path(tunaatlas_qa_global_datasets_catch_path, "entities"), full.names = TRUE, recursive = FALSE)
 
@@ -156,22 +108,21 @@ for (entitynumber in 1:length(config$metadata$content$entities)){
   dataset_pid <- entity$identifiers[["id"]]
   setwd(file.path(file.path,"entities", dataset_pid))
   action <- entity$data$actions[[1]]
-  convert_to_netcdf(action, config, entity)
+  convert_to_netcdf(action, config, entity, uploadgoogledrive = TRUE)
 } #could also be in global action but keep in mind it is very long
 setwd(wd)
 
+# Checking on created data
 
+
+
+# Check on georef not nominal etc.
+
+
+# Putting dataset on geoserver, geonetwork and zenodo
+tunaatlas_qa_services <- executeWorkflow("tunaatlas_qa_services.json")
+
+# Enriching data with copernicus data
 all_files <- list.files(getwd(), pattern = "\\.nc$", full.names = TRUE, recursive = TRUE)
 netcdf_file_to_enrich <- all_files[!grepl("nominal", all_files)]
 
-# Some checking on created data
-
-#check on georef not nominal etc.
-
-
-# executeWorkflow(here("tunaatlas_qa_global_datasets_catch_new.json"))
-
-# executeWorkflow("tunaatlas_qa_global_datasets_effort.json", dir = "jobs/tunaatlas_qa_global_datasets_effort")
-
-dir.create("jobs/tunaatlas_qa_services")
-executeWorkflow("tunaatlas_qa_services.json")
