aa19c888be97cb705c801b0b7c209e808d236c21
bastienird <bastien.grasset@ird.fr>
2024-04-02 14:39:53 +0000

    adding the possibility to cache to create level 1 and 2 on the basis of level 0.

--- tunaatlas_scripts/generation/create_global_tuna_atlas_dataset_v2023.R
+++ tunaatlas_scripts/generation/create_global_tuna_atlas_dataset_v2023.R
@@ -1,7 +1,104 @@
 function(action, entity, config) {
+  
   opts <- action$options
   con <- config$software$output$dbi
   options(encoding = "UTF-8")
+  
+  if(TRUE){#(!is.null(opts$cache)) && opts$cache){
+    
+    escape <- function(x) {
+      gsub("([][{}()+*^$|\\\\?.])", "\\\\\\1", x)
+    }
+    # Get the path to the 'jobs' directory
+    ancient_dir <- here::here("jobs")
+    # Ask user which directory to copy and then copy it to a new directory
+    copy_selected_directory <- function(ancient_dir, entity_name, new_dir) {
+      all_dirs <- list.dirs(path = ancient_dir, full.names = TRUE, recursive = TRUE)
+      matching_dirs <- all_dirs[grep(sprintf("/%s$", entity_name), all_dirs)]
+      
+      if (length(matching_dirs) == 0) {
+        cat("No directories matching the entity name were found.\n")
+        return(NULL)
+      }
+      lines <- ""
+      for (i in seq_along(matching_dirs)) {
+        lines <- paste0(lines,cat(sprintf("[%d] %s\n", i, matching_dirs[i])))
+      }
+      cat("Please select the directory to copy by entering its number:\n")
+      
+      start_time <- proc.time()
+      repeat {
+        answer <- as.integer(readline(prompt = paste0(lines, "\n Enter the number of the directory to copy (e.g., 1, 2, 3):\n Enter 0 if you do not want to use cache and begin from scratch ")))
+        elapsed_time <- proc.time() - start_time
+        
+        if (!is.na(answer) && answer %in% c(0, seq_along(matching_dirs))) {
+          break
+        } else if (elapsed_time[3] >= 30) { # Check if 30 seconds have passed
+          cat("Timeout reached. Proceeding as if 1 was entered.\n")
+          answer <- 1
+          break
+        } else {
+          cat("Invalid input. Proceeding without using cache.\n")
+          answer <- 0
+        }
+      }
+      
+      if (answer == 0) {
+        cat("No directory selected. Proceeding without using cache.\n")
+        return(NULL)
+      } else {
+        # Proceed with copying the selected directory
+        selected_dir <- matching_dirs[answer]
+        if (!dir.exists(new_dir)) {
+          dir.create(new_dir, recursive = TRUE)
+        }
+        
+        # Get the list of all files in the selected directory, recursively
+        files <- list.files(selected_dir, recursive = TRUE, full.names = TRUE)
+        
+        # Order files by modification time
+        files_ordered <- files[order(file.info(files)$mtime)]
+        
+        # Copy the files in the ordered manner
+        for (file_path in files_ordered) {
+          # Determine the relative path
+          relative_path <- gsub(paste0(escape(selected_dir), "/"), "", file_path)
+          dest_path <- file.path(new_dir, relative_path)
+          
+          # Ensure the destination directory exists
+          if (!dir.exists(dirname(dest_path))) {
+            dir.create(dirname(dest_path), recursive = TRUE)
+          }
+          
+          # Copy the file
+          file.copy(file_path, dest_path)
+        }
+        config$logger.info(sprintf("Directory '%s' copied to '%s'.\n", selected_dir, new_dir))
+        cat(sprintf("Directory '%s' copied to '%s'.\n", selected_dir, new_dir))
+      }
+    }
+    
+    new_directory <- getwd()
+    copy_selected_directory(ancient_dir, entity$identifiers[["id"]], new_directory)
+    output_name_dataset <- file.path("data", paste0(entity$identifiers[["id"]], "_harmonized.csv"))
+    output_name_dataset_public <- file.path("data", paste0(entity$identifiers[["id"]], "_public.csv"))
+    output_name_codelists <- file.path("data", paste0(entity$identifiers[["id"]], "_codelists.csv"))
+    
+    entity$addResource("harmonized", output_name_dataset)
+    entity$addResource("public", output_name_dataset_public)
+    entity$addResource("codelists", output_name_codelists)
+    entity$addResource("geom_table", opts$geom_table)
+    #### END
+    config$logger.info(
+      "-----------------------------------------------------------------------------------------------------"
+    )
+    config$logger.info("End: Your tuna atlas dataset has been created!")
+    config$logger.info(
+      "-----------------------------------------------------------------------------------------------------"
+    )
+    return("Cached")
+    
+  } else {
   ######################################################################
   ##### 52North WPS annotations ##########
   ######################################################################
@@ -1271,4 +1368,5 @@ function(action, entity, config) {
   rm(georef_dataset)
   
   gc()
+  }
 }
