e02ad52233ccac70bfdf66d711c34f1826e78375
bastienird <bastien.grasset@ird.fr>
2024-05-13 06:54:59 +0000

    fix time_end equal to time_start (bug certeanly due to new version of lubridate)

--- sardara_functions/format_time_db_format.R
+++ sardara_functions/format_time_db_format.R
@@ -10,7 +10,7 @@ format_time_db_format = function (df_input)
   }
   
   df_input$time_start <- as.Date(paste(df_input$Year, "-", df_input$MonthStart, "-01", sep = ""))
-  df_input$time_end <- as.Date(df_input$time_start) + lubridate::month(df_input$Period) - lubridate::days(1)
+  df_input$time_end <- as.Date(df_input$time_start + months(1) - days(1))
   df_input$time_start <- as.character(df_input$time_start)
   df_input$time_end <- as.character(df_input$time_end)
   return(df_input)
