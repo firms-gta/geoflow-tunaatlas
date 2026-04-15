0eecec2255288f4e80e541b69b5de18e569bbef9
bastienird <bastien.grasset@ird.fr>
2026-04-15 10:02:39 +0000

    fix: hardcoded choosing of datasets recoded ton include all datasets and the tidying is made after

--- R/tunaatlas_scripts/generation/get_rfmos_datasets_level0.R
+++ R/tunaatlas_scripts/generation/get_rfmos_datasets_level0.R
@@ -10,7 +10,7 @@ get_rfmos_datasets_level0 <- function(rfmo, entity, config, options){
   }
   
   #list of dataset files (from entity data sources)
-  dataset_files <- sapply(entity$data$source[2:length(entity$data$source)], function(x){ entity$getJobDataResource(config, x) })
+  dataset_files <- sapply(entity$data$source[1:length(entity$data$source)], function(x){ entity$getJobDataResource(config, x) })
   dataset_files <- sapply(
     entity$data$source[2:length(entity$data$source)],
     function(x) {
