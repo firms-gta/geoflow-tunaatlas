d0205bc45dc3a42d82df1c24642b428b54311f3b
BastienGrassetIRD <bastien.grasset@ird.fr>
2026-09-21 18:26:50 +0400

    feature: removing aggregation as not usefull and too RAM consuming, update structure, handling codelists locally

--- R/tunaatlas_scripts/generation/process_and_aggregate_dataset.R
+++ R/tunaatlas_scripts/generation/process_and_aggregate_dataset.R
@@ -11,24 +11,23 @@
 #'
 #' @return No return value. The function performs operations and exports files directly.
 #' @export
-process_and_aggregate_dataset <- function(georef_dataset, entity, config, opts, 
+process_and_aggregate_dataset <- function(dataset, entity, config, opts, 
   columns_to_keep = c("source_authority", "species", "gear_type", "fishing_fleet", "fishing_mode", "time_start", "time_end", "year", "month", "quarter", "geographic_identifier", "measurement_unit", "measurement_value", 
                       "measurement", "measurement_processing_level")) {
-  
-  # Aggregation by dimensions
-  dataset <- georef_dataset %>%
-    dplyr::group_by(.dots = setdiff(colnames(georef_dataset), "measurement_value")) %>%
-    dplyr::summarise(measurement_value = sum(measurement_value)) %>%
-    # head(1000) %>%
-    dplyr::ungroup()
-  
-  dataset <- as.data.frame(dataset)
+
+  # # Aggregation by dimensions
+  # dataset <- georef_dataset %>%
+  #   dplyr::group_by(.dots = setdiff(colnames(georef_dataset), "measurement_value")) %>%
+  #   dplyr::summarise(measurement_value = sum(measurement_value)) %>%
+  #   # head(1000) %>%
+  #   dplyr::ungroup()
+  # 
+  # dataset <- as.data.frame(dataset)
   
   # Correct measurement unit if necessary
-  if (!is.na(any(dataset$measurement_unit == "TRUE"))) {
-    if (any(dataset$measurement_unit == "TRUE")) {
-      dataset[dataset$measurement_unit == "TRUE", ]$measurement_unit <- "t" # Patch issue
-    }
+  idx <- which(dataset$measurement_unit == "TRUE")
+  if (length(idx) > 0) {
+    dataset$measurement_unit[idx] <- "t"
   }
   
   # Convert time columns to Date format
@@ -39,60 +38,66 @@ process_and_aggregate_dataset <- function(georef_dataset, entity, config, opts,
   dataset_temporal_extent <- paste(as.character(min(dataset$time_start)), as.character(max(dataset$time_end)), sep = "/")
   entity$setTemporalExtent(dataset_temporal_extent)
   
-  # Append codelists if available
-  df_codelists <- NULL
-  cl_relations <- entity$relations[sapply(entity$relations, function(x) x$name == "codelists")]
+  dataset_files <- sapply(
+    entity$data$source,
+    function(x) entity$getJobDataResource(config, x)
+  )
   
-  if (length(cl_relations) > 0) {
-    config$logger.info("Appending codelists to global dataset generation action output")
-    googledrive_baseurl <- "https://drive.google.com/open?id="
+  codelists_path <- dataset_files[
+    grepl("codelist", basename(dataset_files), ignore.case = TRUE)
+  ][1]
+  
+  if (file.exists(codelists_path)) {
+    log_info(
+      paste0(
+        "Loading global codelists from: ",
+        codelists_path
+      )
+    )
     
-    if (startsWith(cl_relations[[1]]$link, googledrive_baseurl)) {
-      # Downloading file using Google Drive R interface
-      config$logger.info("Downloading file using Google Drive R interface")
-      drive_id <- unlist(strsplit(cl_relations[[1]]$link, "id="))[2]
-      drive_id <- unlist(strsplit(drive_id, "&export"))[1] # Control in case export param is appended
-      googledrive::drive_download(file = googledrive::as_id(drive_id), path = file.path("data", paste0(entity$identifiers[["id"]], "_codelists.csv")), overwrite = TRUE)
-      df_codelists <- read.csv(file.path("data", paste0(entity$identifiers[["id"]], "_codelists.csv")))
-    } else {
-      df_codelists <- read.csv(cl_relations[[1]]$link)
-    }
+    df_codelists <- read.csv(codelists_path)
+    
+  } else {
+    config$logger.warn(
+      paste0(
+        "Global codelists file not found: ",
+        codelists_path,
+        ". The dataset will be generated without codelist metadata. ",
+        "The codelists file is required by load_dataset() to associate ",
+        "each dimension with its corresponding global codelist. ",
+        "The workflow will therefore fail if this dataset is later loaded into the database."
+      )
+    )
+    
+    df_codelists <- NULL
   }
   
-  # Output structure
-  dataset_list <- list(
-    dataset = dataset,
-    additional_metadata = NULL,
-    codelists = df_codelists # In case the entity was provided with a link to codelists
-  )
-  dataset_list$dataset$geographic_identifier = as.character(dataset_list$dataset$geographic_identifier)
+  dataset$geographic_identifier = as.character(dataset$geographic_identifier)
   # Export dataset as CSV
   output_name_dataset <- file.path("data", paste0(entity$identifiers[["id"]], "_harmonized.csv"))
-  readr::write_csv(dataset_list$dataset, output_name_dataset)
+  readr::write_csv(dataset, output_name_dataset)
   
   # Create enriched public dataset
   output_name_dataset_public <- file.path("data", paste0(entity$identifiers[["id"]], "_public.csv"))
-  dataset_enriched <- dataset_list$dataset #%>% head(1000)
-  dataset_enriched$year <- as.integer(format(dataset_enriched$time_end, "%Y"))
-  dataset_enriched$month <- as.integer(format(dataset_enriched$time_end, "%m"))
-  dataset_enriched$quarter <- as.integer(substr(quarters(dataset_enriched$time_end), 2, 2))
+  dataset$year <- as.integer(format(dataset$time_end, "%Y"))
+  dataset$month <- as.integer(format(dataset$time_end, "%m"))
+  dataset$quarter <- as.integer(substr(quarters(dataset$time_end), 2, 2))
   
   
-  columns_to_keep <- intersect(colnames(dataset_enriched), columns_to_keep)
+  columns_to_keep <- intersect(colnames(dataset), columns_to_keep)
   
-  dataset_enriched <- dataset_enriched[, columns_to_keep]
-  readr::write_csv(dataset_enriched, output_name_dataset_public)
+  dataset <- dataset[, columns_to_keep]
+  readr::write_csv(dataset, output_name_dataset_public)
   
   # Export codelists if available
   if (!is.null(df_codelists)) {
     output_name_codelists <- file.path("data", paste0(entity$identifiers[["id"]], "_codelists.csv"))
-    write.csv(dataset_list$codelists, output_name_codelists, row.names = FALSE)
+    write.csv(df_codelists, output_name_codelists, row.names = FALSE)
     entity$addResource("codelists", output_name_codelists)
   }
   
   #write to service dbi
-  dataset_enriched$geographic_identifier = as.character(dataset_enriched$geographic_identifier)  
-  entity$data$features = dataset_enriched
+  entity$data$features = dataset
   # if(entity$data$upload) writeWorkflowJobDataResource(entity=entity,config=config,type="dbtable",useFeatures=TRUE,useUploadSource=TRUE, createIndexes=TRUE)
   entity$addResource("fact", opts$fact)
   entity$addResource("geom_table", opts$geom_table)
@@ -102,11 +107,12 @@ process_and_aggregate_dataset <- function(georef_dataset, entity, config, opts,
   entity$addResource("public", output_name_dataset_public)
   
   # Log completion
-  config$logger.info("-----------------------------------------------------------------------------------------------------")
-  config$logger.info("End: Your tuna atlas dataset has been created!")
-  config$logger.info("-----------------------------------------------------------------------------------------------------")
+  log_info("-----------------------------------------------------------------------------------------------------")
+  log_info("End: Your tuna atlas dataset has been created!")
+  log_info("-----------------------------------------------------------------------------------------------------")
   
   # Clean up
-  rm(georef_dataset)
+  rm(dataset)
   gc()
+  return(NULL)
 }
